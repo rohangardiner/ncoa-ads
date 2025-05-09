1bc980363439b0848d91cae39741a8eb8dcb7f51
Rohan <71481034+rohangardiner@users.noreply.github.com>
2025-05-09 10:57:21 +1000

    added release builder

--- ncoa-ads.php
+++ ncoa-ads.php
@@ -166,8 +166,8 @@ function run_ncoa_ads() {
             'github_url' => 'https://github.com/rohangardiner/ncoa-ads', // the GitHub url of your GitHub repo
             'zip_url' => 'https://github.com/rohangardiner/ncoa-ads/release/ncoa-ads.zip', // the zip url of the GitHub repo
             'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-            'requires' => '3.0', // which version of WordPress does your plugin require?
-            'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+            'requires' => '6.0', // which version of WordPress does your plugin require?
+            'tested' => '6.8.1', // which version of WordPress is your plugin tested up to?
             'readme' => 'README.md', // which file to use as the readme for the version number
             'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
          );
